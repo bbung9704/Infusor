69e85fc5d01692143d088801e1853fa757ad413c
Taeyoon <bbung9704@naver.com>
2023-09-14 00:05:30 +0900

    [webcam] mirrored 수정

--- frontend/src/pages/WebCame.js
+++ frontend/src/pages/WebCame.js
@@ -73,7 +73,7 @@ const WebCam = () => {
                         screenshotQuality={1}
                         minScreenshotWidth={4096}
                         minScreenshotHeight={4096}
-                        mirrored={true}
+                        mirrored={false}
                         style={webcamStyle}
                     />
                 )}
