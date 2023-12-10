ec789618817fc3fae53c4d2ce4537266466f140b
Taeyoon <bbung9704@naver.com>
2023-12-10 15:04:05 +0900

    volume estimation algorithm update

--- backend/predict_volume.py
+++ backend/predict_volume.py
@@ -7,30 +7,39 @@ def cal_iou(tgt_np, compare):
     iou = overlap.sum()/float(union.sum())
     return iou
 
+# def predict_volume(tgt):
+#     result = [0 for _ in range(23)]
+#     cnt = [0 for _ in range(23)]
+
+#     tgt_np = np.array(tgt, dtype=bool)
+
+#     root = 'images/label'
+#     labels = os.listdir(root)
+#     for label in labels:
+#         file_name = label.split('.')[0]
+#         volume = file_name.split('_')[-1]
+#         idx = int(int(volume) / 10)
+
+#         img = cv2.imread(root + '/' + label)
+#         compare = np.array(img, dtype=bool)
+#         iou = cal_iou(tgt_np, compare)
+
+#         cnt[idx] += 1
+#         result[idx] += iou
+
+#     for i in range(len(result)):
+#         result[i] /= cnt[i]
+
+#     max_iou = max(result)
+#     predict = result.index(max_iou) * 10
+
+#     return predict
+
 def predict_volume(tgt):
-    result = [0 for _ in range(23)]
-    cnt = [0 for _ in range(23)]
+    tgt_np = np.array(tgt)
+    tgt_area = np.sum(tgt_np)
+    coef = 0.0007725
+    intercept = -42.33394674384486
 
-    tgt_np = np.array(tgt, dtype=bool)
-
-    root = 'images/label'
-    labels = os.listdir(root)
-    for label in labels:
-        file_name = label.split('.')[0]
-        volume = file_name.split('_')[-1]
-        idx = int(int(volume) / 10)
-
-        img = cv2.imread(root + '/' + label)
-        compare = np.array(img, dtype=bool)
-        iou = cal_iou(tgt_np, compare)
-
-        cnt[idx] += 1
-        result[idx] += iou
-
-    for i in range(len(result)):
-        result[i] /= cnt[i]
-
-    max_iou = max(result)
-    predict = result.index(max_iou) * 10
-
+    predict = coef * tgt_area + intercept
     return predict
